b314d73c53c6856b62ceac23ec62af6a26006cdf
Beth Hansen and Shyamal Punekar <beth+shyamal@>
2017-07-19 16:32:57 -0700

    Add branching logic inside prototype

--- js/scripts.js
+++ js/scripts.js
@@ -6,10 +6,18 @@ function LetsPlay () {
 
   function Dice () {
     sides = 1;
+    this.dieOne = 1;
+    this.dieValue = 0;
+    this.dieOne = Math.floor(Math.random() * 6) + 1;
   }
-  Dice.prototype.rollDice = function () {
-    return Math.floor(Math.random() * 6) + 1;
-  }
+
+  var newDice = new Dice();
+
+  // Dice.prototype.rollDice = function () {
+  //   this.dieOne = parseInt(Math.floor(Math.random() * 6) + 1);
+  //   // alert(this.dieOne);
+  //   return this.dieOne;
+  // }
 
 function Player (userName) {
   this.userName = userName;
@@ -18,25 +26,18 @@ function Player (userName) {
   this.isTurn =false;
 }
 
-Player.prototype.addItUp = function(input) {
-  this.turnScore += input;
+Player.prototype.gameResult = function() {
+  // this.gameDie;
+  Dice();
+  alert(this.dieOne);
+  if(this.dieOne === 1){
+    alert("hi");
+  return this.dieValue = 0;
+} else {
+  return this.dieValue += this.dieOne;
+  }
 }
-
-  // Dice.prototype.rollDice = function(){
-  //   return Math.floor(Math.random() * 6) + 1;
-    // this.diceOne = Math.floor(Math.random() * 6) + 1;
-    //
-    // alert(this.diceOne);
-  // }
-
-  // Dice.prototype.gameResult = function(){
-  //   if(this.diceOne === 1){
-  //     return this.turnScore = 0;
-  //     // this.isTurn =false;
-  //   }
-  // }
 }
-
 
 // User Interface Logic
 $(document).ready(function() {
@@ -45,21 +46,15 @@ $(document).ready(function() {
   $("#game-action").submit(function(event) {
     event.preventDefault();
 
-    var thisRoll = newGame.gameDie.rollDice();
-
-    newGame.player1.addItUp(thisRoll);
+    // var thisRoll = newGame.gameDie.rollDice();
+    var thisGameResult = newGame.player1.gameResult();
+    // newGame.player1.gameResult(thisGameResult);
     // console.log(thisRoll);
-    // if (thisRoll === 1) {
-    //   alert('hello');
-    // }
 
     // var inputFirstPlayer = $("#firstPlayer").val();
     var inputFirstPlayerName = $("#FirstPlayerName").val();
 
-    // var newDice = new Dice();
-    // newDice.gameResult();
-
-    // $(".player1Result").text(newDice.turnScore);
+    $(".player1Result").text(thisGameResult);
     $("#showResult").show();
   });
 });
